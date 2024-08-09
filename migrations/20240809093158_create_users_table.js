310454e25b66e395d3372643cdd219be93ddd8a3
Simo Pietikäinen <82802222+pietikainen@users.noreply.github.com>
2024-08-09 12:53:53 +0300

    revisit users table

---
+++ migrations/20240809093158_create_users_table.js
@@ -0,0 +1,26 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.createTable('users', (table) => {
+      table.increments('id').primary();
+      table.integer('snowflake').unique().notNullable();
+      table.string('username').unique().notNullable();
+      table.string('nickname');
+      table.string('avatar');
+      table.string('email');
+      table.integer('phone');
+      table.string('bankaccount');
+      table.integer('userlevel').notNullable();
+      table.timestamps(true, true);
+    });
+  };
+  
+  /**
+   * @param { import("knex").Knex } knex
+   * @returns { Promise<void> }
+   */
+  exports.down = function(knex) {
+    return knex.schema.dropTable('users');
+  };
