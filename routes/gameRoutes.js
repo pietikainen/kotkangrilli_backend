7b17db1d790fe84c4386971ad0ccb4be8a22c18a
Simo Pietikäinen <82802222+pietikainen@users.noreply.github.com>
2024-09-01 21:25:38 +0300

    middleware to check igdb token expiry

--- routes/gameRoutes.js
+++ routes/gameRoutes.js
@@ -2,12 +2,27 @@
 const express = require('express');
 const router = express.Router();
 const gameController = require('../controllers/gameController');
+const configController = require('../controllers/configController');
+
+
+// Middleware to check IDGB token expiry date - if old, get new.
+
+router.use((req, res, next) => {
+    const now = new Date();
+    const data = configController.getIgdbTokenFromConfig(req, res);
+
+    const expiryDate = data.expiry;
+
+    if (now > expiryDate) {
+        gameController.updateIgdbTokenInConfig();
+    }
+    next();
+})
+
+router.get('/store-url/:id', gameController.getGameStoreUrl);
 
 // POST route to add a new game
 router.post('', gameController.addGame);
-
-// Bypass middleware for API testing purposes
-// router.post('/games', gameController.addGame);
 
 // GET route to get all games
 router.get('', gameController.getAllGames);
