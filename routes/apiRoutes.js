42088110a78bbeb48ba4a498e40b8d7137c3d3db
Simo Pietikäinen <82802222+pietikainen@users.noreply.github.com>
2024-09-03 10:37:55 +0300

    move middleware up

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,15 +4,18 @@ const authMiddleware = require('../middleware/authMiddleware');
 
 
 // test token refresh route
-router.use('/config', require('./configRoutes'));
-
-
-router.use('/games', require('./gameRoutes'));
 router.use(authMiddleware.ensureAuthenticated);
+
+
+router.use('/config', require('./configRoutes'));
+router.use('/games', require('./gameRoutes'));
 
 router.use('/events', require('./eventRoutes'));
 router.use('/users', require('./userRoutes'));
 router.use('/participations', require('./participationRoutes'));
+
+
+
 
 
 module.exports = router;
