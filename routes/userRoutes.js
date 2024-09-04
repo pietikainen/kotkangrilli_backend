de56ce06c1211083e3883319fc9632940ec7e9fa
Simo Pietikäinen <82802222+pietikainen@users.noreply.github.com>
2024-09-04 16:36:47 +0300

    move endpoint from admin to users

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,10 +5,17 @@ const express = require('express');
 
 const router = express.Router();
 const userController = require('../controllers/userController');
+const adminMiddleware = require('../middleware/adminMiddleware');
+const adminController = require('../controllers/adminController');
+
 
 router.get('/me', userController.getUser);
 router.get('', userController.getAllUsers);
 
 router.get('/user-profiles', userController.getAllUserProfiles);
+
+router.use(adminMiddleware.ensureAdmin);
+router.patch('/:userId/userlevel', adminController.updateUserRole);
+
 
 module.exports = router;
