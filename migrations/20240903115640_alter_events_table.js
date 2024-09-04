c277fc9d9825e43942cd72d5ce8761b7e732260c
Simo Pietikäinen <82802222+pietikainen@users.noreply.github.com>
2024-09-04 16:36:56 +0300

    db fixes

---
+++ migrations/20240903115640_alter_events_table.js
@@ -0,0 +1,18 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.alterTable('events', (table) => {
+    table.timestamp('startDate').notNullable().alter();
+    table.timestamp('endDate').notNullable().alter();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  
+};
