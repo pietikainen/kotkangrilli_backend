60fd925c9d4fd9b711754e6743bf45b6e938f7f1
Simo Pietikäinen <82802222+pietikainen@users.noreply.github.com>
2024-09-05 17:17:26 +0300

    alter events table to have winnergamescount

---
+++ migrations/20240905130013_alter_events_table.js
@@ -0,0 +1,18 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.alterTable('events', (table) => {
+        table.integer('winnerGamesCount').defaultTo(4);
+
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  
+};
