92b0bee36a4d430c26dba1fd818ba988be02c247
Simo Pietikäinen <82802222+pietikainen@users.noreply.github.com>
2024-09-18 22:51:27 +0300

    add voteController and voteRoutes

---
+++ controllers/voteController.js
@@ -0,0 +1,116 @@
+// voteController.js
+
+const Vote = require('../models/Vote');
+const Participation = require('../models/Participation');
+const authMiddleware = require('../middleware/authMiddleware');
+const {isAdmin} = require("../middleware/authMiddleware");
+
+
+// POST: Cast a vote
+exports.castVote = async (req, res) => {
+    const eventId = req.params.eventId;
+    const userId = 1;
+    const gameId = req.params.gameId;
+
+    try {
+        const checkForExisting = await Vote.query()
+            .select('id')
+            .where('eventId', eventId)
+            .andWhere('gameId', gameId)
+            .andWhere('userId', userId);
+
+        const isUserRegistered = await Participation.query()
+            .select('id')
+            .where('eventId', eventId)
+            .andWhere('userId', userId);
+
+        // EI TOIMI VIELÄ
+        if (isUserRegistered != 0) {
+            return res.status(403).json({
+                success: false,
+                message: 'User is not registered to the event'
+            })
+        }
+
+        if (checkForExisting != 0) {
+            return res.status(400).json({
+                success: false,
+                message: "Vote already exists"
+            });
+        }
+
+        const newVote = await Vote.query().insert({
+            eventId,
+            userId,
+            gameId
+        });
+
+
+        if (!newVote) {
+            return res.status(400).json({
+                success: false,
+                message: 'Error casting vote'
+            });
+        } else {
+            return res.status(201).json({
+                success: true,
+                data: newVote
+            });
+        }
+
+    } catch (error) {
+        console.log("error: ", error.message);
+        res.status(500).json({
+            success: false,
+            message: 'Error adding vote',
+            error: error.message
+        })
+    }
+}
+
+// DELETE: Delete a vote by voteId
+exports.deleteVote = async (req, res) => {
+
+    const voteId = req.params.voteId;
+    const userId = 6;
+
+    try {
+        // Check if user is the original voter
+
+        const isOriginalVoter = await Vote.query()
+            .where('id', voteId)
+            .andWhere('userId', userId);
+
+        console.log("isOriginalVoter", isOriginalVoter);
+        console.log("userid", userId);
+        console.log("isoriginal.userid", isOriginalVoter.userId);
+        if (userId != isOriginalVoter.userId) {
+        // if (!isOriginalVoter && !isAdmin()) {
+            return res.status(403).json({
+                success: false,
+                message: 'Error: User mismatch'
+            });
+        }
+
+        const deleteVote = await Vote.query().deleteById(voteId);
+
+
+
+
+
+
+        if (deleteVote) {
+            return res.status(200).json({
+                success: true,
+            })
+        }
+    } catch (error) {
+        console.log("error: ", error.message);
+        return res.status(500).json({
+            success: false,
+            message: 'Error deleting vote',
+            error: error.message
+        });
+    }
+
+}
--- routes/voteRoutes.js
+++ routes/voteRoutes.js
@@ -4,13 +4,14 @@ const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middleware/authMiddleware');
 const adminController = require('../controllers/adminController');
-// const voteController = require('../controllers/voteController');
+const voteController = require('../controllers/voteController');
 
 // POST route to add a new vote
+router.post('/:eventId/:gameId', voteController.castVote);
 
 // OBS! Not for use.
 // GET: Calculate votes and update winners with amount of param (int) (ADMIN ONLY)
-router.get('/calculate', authMiddleware.isAdmin, adminController.calculateVotes);
+router.get('/calculate/:limit', authMiddleware.isAdmin, adminController.calculateVotes);
 
 // GET route to get all votes by userID
 
@@ -21,6 +22,7 @@ router.get('/calculate', authMiddleware.isAdmin, adminController.calculateVotes)
 // PATCH route to update a vote
 
 // DELETE route to delete a vote
+router.delete('/:voteId', voteController.deleteVote);
 
 module.exports = router;
 
