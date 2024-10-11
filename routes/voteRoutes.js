4a0bbf611eb837bb3666cb054e8e00915619858e
Lartza <Lartza@users.noreply.github.com>
2024-10-11 15:19:39 +0300

    Allow votecount for all users

--- routes/voteRoutes.js
+++ routes/voteRoutes.js
@@ -19,7 +19,7 @@ router.post('/:eventId/:gameId', voteController.castVote);
 // OBS! Not for use.
 // GET: Calculate votes and update winners with amount of param (int) (ADMIN ONLY)
 router.get('/calculate/:limit', isAdmin, adminController.calculateVotes);
-router.get('/count/:eventId', isAdmin, adminController.countVotesByEvent);
+router.get('/count/:eventId', adminController.countVotesByEvent);
 
 // GET route to get all votes per Event ID by req.user.id
 router.get('/:eventId/', voteController.getVotesByUser);
